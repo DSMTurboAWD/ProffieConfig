9a0bacfbd79e78c0dd03462083ed6bc9b4260475
ryancog <170381220+ryancog@users.noreply.github.com>
2026-03-10 03:31:38 -0400

    [UI] Add Segmented; Untested

---
+++ src/components/ui/controls/segmented.cpp
@@ -0,0 +1,137 @@
+#include "segmented.hpp"
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2026 Ryan Ogurek
+ *
+ * components/ui/controls/segmented.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/tglbtn.h>
+
+#include "data/helpers/exclusive.hpp"
+#include "ui/priv/helpers.hpp"
+#include "ui/priv/winbase.hpp"
+
+using namespace pcui;
+
+/*
+ * This is a segmented control in spirit...
+ *
+ * Making it a proper-looking one would be nice. I know one exists for macOS
+ * and probably GTK... not sure about Windows (probably not).
+ *
+ * In any case, afaik it's not something wxWidgets exposes, so it'd be
+ * non-trivial to add.
+ */
+
+namespace {
+
+struct Control : priv::WinBase<wxToggleButton, data::Model::Receiver> {
+    Control(
+        wxWindow *parent,
+        const Segmented::Label& label,
+        data::Bool& data,
+        const detail::ChildWindowBase& win
+    ) {
+        Create(parent, wxID_ANY, label.text_);
+        if (label.image_.IsOk()) SetBitmap(label.image_);
+
+        postCreation(win);
+
+        attach(data);
+    }
+
+    ~Control() override {
+        detach();
+    }
+
+    friend struct Manager;
+};
+
+struct Manager : priv::WinBase<wxPanel, data::Exclusive::Receiver> {
+    Manager(wxWindow *parent, const Segmented& desc) {
+        Create(parent);
+        auto *sizer{new wxBoxSizer(wxHORIZONTAL)};
+
+        postCreation(desc.win_);
+
+        for (auto idx{0}; idx < desc.data_.data().size(); ++idx) {
+            auto& bl{*desc.data_.data()[idx]};
+            auto *ctrl{new Control(
+                this, desc.labels_[idx], bl, desc.win_
+            )};
+
+            sizer->Add(ctrl);
+        }
+
+        SetSizer(sizer);
+
+        attach(desc.data_);
+        Bind(wxEVT_RADIOBUTTON, &Manager::onSet, this);
+    }
+
+    ~Manager() override {
+        Unbind(wxEVT_RADIOBUTTON, &Manager::onSet, this);
+        detach();
+    }
+
+    void onSet(wxCommandEvent& evt) {
+        for (auto *child : GetChildren()) {
+            if (child != evt.GetEventObject()) continue;
+
+            auto& bl{const_cast<data::Bool&>(
+                static_cast<Control *>(child)->model<data::Bool>()
+            )};
+            auto res{bl.processUIAction(
+                std::make_unique<data::Bool::SetAction>(true)
+            )};
+
+            if (not res) {
+                auto selected{model<data::Exclusive>().selected()};
+                auto *child{GetChildren()[selected]};
+
+                static_cast<Control *>(child)->SetValue(true);
+            }
+            break;
+        }
+    }
+    
+    void onSelection(size idx) override {
+        CallAfter([this, idx] {
+            auto *child{GetChildren()[idx]};
+            static_cast<wxToggleButton *>(child)->SetValue(true);
+        });
+    }
+};
+
+} // namespace
+
+std::unique_ptr<detail::Descriptor> Segmented::operator()() {
+    return std::make_unique<Segmented::Desc>(std::move(*this));
+}
+
+Segmented::Desc::Desc(Segmented&& data) :
+    Segmented{std::move(data)} {}
+
+wxSizerItem *Segmented::Desc::build(const detail::Scaffold& scaffold) const {
+    auto *chk{new Manager(scaffold.childParent_, *this)};
+    auto *item{new wxSizerItem(chk)};
+    priv::apply(base_, item);
+    return item;
+}
+
---
+++ src/components/ui/controls/segmented.hpp
@@ -0,0 +1,57 @@
+#pragma once
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2026 Ryan Ogurek
+ *
+ * components/ui/controls/segmented.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "data/helpers/exclusive.hpp"
+#include "ui/detail/descriptor.hpp"
+#include "ui/detail/general.hpp"
+#include "ui/types.hpp"
+
+#include "ui_export.h"
+
+namespace pcui {
+
+struct UI_EXPORT Segmented {
+    struct Desc;
+
+    // TODO: Make this a base w/ C++ P2287.
+    detail::ChildBase base_;
+    detail::ChildWindowBase win_;
+
+    data::Exclusive& data_;
+
+    struct Label {
+        wxString text_;
+        wxBitmap image_;
+    };
+
+    std::vector<Label> labels_;
+
+    DescriptorPtr operator()();
+};
+
+struct UI_EXPORT Segmented::Desc : Segmented, detail::Descriptor {
+    Desc(Segmented&&);
+
+    [[nodiscard]] wxSizerItem *build(const detail::Scaffold&) const override;
+};
+
+} // namespace pcui
+
